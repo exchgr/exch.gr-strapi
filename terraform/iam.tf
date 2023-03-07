0d0d393fb8b845c40c441782fd8a398da454303e
Elle Mundy <exchgr@icloud.com>
2023-03-07 18:06:40 -0500

    Attach policies for cluster and node groups
    Gives the cluster role these permissions:
    - creating and identifying resources on ec2
    
    Gives the node group role these permissions:
    - connecting to eks clusters
    - read-only ecr access
    - list/describe/modify ENIs/CNIs

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -12,6 +12,11 @@ resource "aws_iam_role" "exch-gr-eks-cluster-role" {
 	})
 }
 
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+	role       = aws_iam_role.exch-gr-eks-cluster-role.name
+}
+
 resource "aws_iam_role" "exch-gr-eks-node-group-role" {
 	name = "exch-gr-eks-node-group-role"
 	assume_role_policy = jsonencode({
@@ -24,4 +29,19 @@ resource "aws_iam_role" "exch-gr-eks-node-group-role" {
 			}
 		}]
 	})
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+	role       = aws_iam_role.exch-gr-eks-node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+	policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+	role       = aws_iam_role.exch-gr-eks-node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+	policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+	role       = aws_iam_role.exch-gr-eks-node-group-role.name
 }
