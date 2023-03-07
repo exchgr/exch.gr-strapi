a775724c32a49f0d481a00948347bb59818b802d
Elle Mundy <exchgr@icloud.com>
2023-03-07 18:31:37 -0500

    Add public and private subnets

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -5,3 +5,25 @@ resource "aws_vpc" "exch-gr" {
 		Name = "exch-gr"
 	}
 }
+
+resource "aws_subnet" "exch-gr-public" {
+	vpc_id = aws_vpc.exch-gr.id
+	cidr_block = "10.0.1.0/24"
+	availability_zone = "us-east-1a"
+	map_public_ip_on_launch = true
+
+	tags = {
+		Name = "exch-gr-public"
+	}
+}
+
+resource "aws_subnet" "exch-gr-private" {
+	vpc_id = aws_vpc.exch-gr.id
+	cidr_block = "10.0.2.0/24"
+	availability_zone = "us-east-1a"
+	map_public_ip_on_launch = false
+
+	tags = {
+		Name = "exch-gr-private"
+	}
+}
