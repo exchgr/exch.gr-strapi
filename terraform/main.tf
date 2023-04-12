9a6c1d0465c6ff7c20dc924f6304a8ae0cb7e758
Elle Mundy <exchgr@icloud.com>
2023-04-12 18:43:52 -0400

    Fetch and use kubeconfig for terraform kubernetes provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,9 +10,18 @@ terraform {
 			source = "hashicorp/aws"
 			version = "~> 4.16"
 		}
+
+		kubernetes = {
+			source = "hashicorp/kubernetes"
+			version = "~> 2.19.0"
+		}
 	}
 }
 
 provider "aws" {
 	region = "us-east-1"
+}
+
+provider "kubernetes" {
+	config_path = "~/.kube/config"
 }
