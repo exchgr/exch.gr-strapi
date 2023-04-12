421a32bec5d2dc770c24d05dd060ccd40289fd32
Elle Mundy <exchgr@icloud.com>
2023-04-12 16:35:01 -0400

    Create iam policy for aws load balancer controller

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -45,3 +45,8 @@ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
 	policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 	role       = aws_iam_role.exch-gr-eks-node-group-role.name
 }
+
+resource "aws_iam_policy" "aws-load-balancer-controller" {
+	name = "aws-load-balancer-controller"
+	policy = file("./aws-load-balancer-controller-iam-policy.json")
+}
