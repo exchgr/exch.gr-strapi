b108d52de012b23dbf5110645e70e01993bf4153
Elle Mundy <exchgr@icloud.com>
2025-01-15 17:17:57 -0500

    destroy all AWS resources

--- terraform/cloudflare_record.tf
+++ terraform/cloudflare_record.tf
@@ -1,19 +1,3 @@
-resource "cloudflare_record" "aws_tls_validation" {
-	name    = replace(
-		element(
-			aws_acm_certificate.aws_acm_certificate.domain_validation_options.*.resource_record_name,
-			0
-		),
-		".${data.external.env.result["DOMAIN_NAME"]}.",
-		""
-	)
-	proxied = false
-	ttl     = 3600
-	type    = element(aws_acm_certificate.aws_acm_certificate.domain_validation_options.*.resource_record_type, 0)
-	value   = element(aws_acm_certificate.aws_acm_certificate.domain_validation_options.*.resource_record_value, 0)
-	zone_id = data.external.env.result["CLOUDFLARE_ZONE_ID"]
-}
-
 resource "cloudflare_record" "admin" {
 	name    = "admin"
 	proxied = false
